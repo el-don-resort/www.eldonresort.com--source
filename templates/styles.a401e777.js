77815b09bef13b0de3eb34447bba4b17c459af87
Tom Claudio <t0mclaudio@Toms-MacBook-Pro.local>
2019-06-22 12:44:22 +0800

    Cleanup: Delete react-static remnants

--- templates/styles.a401e777.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{120:function(n,o,c){},122:function(n,o,c){},123:function(n,o,c){},125:function(n,o,c){}}]);
-//# sourceMappingURL=styles.a401e777.js.map
